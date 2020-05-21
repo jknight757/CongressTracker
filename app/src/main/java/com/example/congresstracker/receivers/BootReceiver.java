b81d78b055ad4a767d9ce15df634f92380f365ca
Jeffrey Knight <Jeffrey.757Knight@gmail.com>
2020-05-21 17:19:31 -0400

    Final

--- app/src/main/java/com/example/congresstracker/receivers/BootReceiver.java
+++ app/src/main/java/com/example/congresstracker/receivers/BootReceiver.java
@@ -23,15 +23,15 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "onReceive: ");
 
-        Constraints constraints = new Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build();
-
-        PeriodicWorkRequest request = new PeriodicWorkRequest
-                .Builder(NotificationService.class, PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MILLISECONDS)
-                .setConstraints(constraints)
-                .build();
-        WorkManager.getInstance(context).enqueue(request);
+//        Constraints constraints = new Constraints.Builder()
+//                .setRequiredNetworkType(NetworkType.CONNECTED)
+//                .build();
+//
+//        PeriodicWorkRequest request = new PeriodicWorkRequest
+//                .Builder(NotificationService.class, PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MILLISECONDS)
+//                .setConstraints(constraints)
+//                .build();
+//        WorkManager.getInstance(context).enqueue(request);
 
 
     }
